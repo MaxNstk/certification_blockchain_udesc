85b3ade8b18a1561ce9552c8602dff86a5a6f73a
Max Starke <maxnatanaelstk@gmail.com>
2024-08-19 20:22:51 -0300

    feat: novos campos no asset de certificado

--- chaincode-typescript/src/certificate.ts
+++ chaincode-typescript/src/certificate.ts
@@ -5,53 +5,65 @@ import { Object, Property } from 'fabric-contract-api';
 @Object()
 export class Certificate {
     @Property()
-    public certificateNumber: string = ''; // Unique identifier for the certificate
+    public certificateNumber: string = ''; 
 
     @Property()
-    public certificateEmissionDate: string = new Date().toISOString(); // Date of certificate issuance (ISO 8601 string)
+    public certificateEmissionDate: string = new Date().toISOString();
 
     @Property()
-    public certificateCourse: string = ''; // Course in which the owner graduated
+    public certificateCourse: string = ''; 
 
     @Property()
-    public certificateStatus: 'valid' | 'revoked' = 'valid'; // Status of the certificate (either 'valid' or 'revoked')
+    public certificateStatus: 'valid' | 'revoked' = 'valid'; 
 
     @Property()
-    public ownerName: string = ''; // Full name of the certificate owner
+    public ownerName: string = '';
 
     @Property()
-    public ownerRG: string = ''; // RG (Registro Geral) of the certificate owner
+    public ownerRG: string = ''; 
 
     @Property()
-    public ownerBirthDate: string = new Date().toISOString(); // Birthdate of the certificate owner (ISO 8601 string)
+    public ownerBirthDate: string = new Date().toISOString(); 
 
     @Property()
-    public ownerBirthState: string = ''; // State where the certificate owner was born
+    public ownerBirthState: string = ''; 
 
     @Property()
-    public campusName: string = ''; // Name of the campus where the certificate owner graduated
+    public campusName: string = ''; 
 
     @Property()
-    public campusAcronym: string = ''; // Acronym of the campus where the certificate owner graduated
+    public campusAcronym: string = ''; 
 
     @Property()
-    public campusDirector: string = ''; // Full name of the campus director
+    public campusDirector: string = ''; 
 
     @Property()
-    public universityPresidentName: string = ''; // Full name of the university president at the time of certificate issuance
+    public universityPresidentName: string = ''; 
 
     @Property()
-    public universityCertificateCoordinator: string = ''; // Full name of the university's certificate coordinator at the time of issuance
+    public universityCertificateCoordinator: string = ''; 
 
     @Property()
-    public hasCompletedAllSubjects: boolean = true; // Verificação se o estudante completou todas as matérias necessárias
+    public hasCompletedAllSubjects: boolean = true;
 
     @Property()
-    public hasSentAllRequiredDocuments: boolean = true; // Verificação se o estudante enviou toda a documentação necessária
+    public hasSentAllRequiredDocuments: boolean = true; 
 
     @Property()
-    public wentToDegreeGranting: boolean = true; // Verificação se o estudante participou da solenidade de outorga de grau
+    public wentToDegreeGranting: boolean = true;
 
     @Property()
-    public note: string = ''; // Campo textual para observações adicionais
+    public note: string = ''; 
+
+    @Property()
+    public creationUser: string = ''; 
+
+    @Property()
+    public creationDate: string = new Date().toISOString(); 
+
+    @Property()
+    public updateUser: string = '';
+
+    @Property()
+    public updateDate: string = new Date().toISOString(); 
 }
